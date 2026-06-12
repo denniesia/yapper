8b7d22b29c057a4a4c82ce513f03c9bd99998798
dennie <denitsa.sureva@icloud.com>
2026-06-12 17:55:21 +0200

    updated tweetcard

--- app/components/tweet/TweetCard.jsx
+++ app/components/tweet/TweetCard.jsx
@@ -2,7 +2,7 @@
 
 import { formatDistanceToNow } from "date-fns";
 import Link from "next/link";
-import { Heart } from "lucide-react";
+import { Heart, MessageCircle } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 
@@ -11,6 +11,7 @@ export default function TweetCard({ tweet }) {
     const { data: session, status } = useSession();
 
     const [isLiked, setIsLiked] = useState(false);
+    const [likesCount, setLikesCount] = useState(tweet.likes.length);
 
     useEffect(() => {
         if (!session?.user?.id) return;
@@ -21,7 +22,6 @@ export default function TweetCard({ tweet }) {
             )
         );
     }, [session, tweet.likes]);
-
 
     const createdTimeAgo = formatDistanceToNow(
         new Date(tweet.createdAt),
@@ -42,43 +42,58 @@ export default function TweetCard({ tweet }) {
         const data = await res.json();
 
         setIsLiked(data.liked);
+        setLikesCount(data.likesCount)
     }
-    
+
     return (
         <div className="p-4 border-b border-gray-800 flex space-x-4 hover:bg-gray-900">
-                <div className="w-12 h-12 bg-gray-700 rounded-full flex-shrink-0">
-                    <Link href={`/users/${tweet.author._id}`}>
-                        <img
-                            className="w-12 h-12 rounded-full object-cover"
-                            src={
-                                tweet.author.image ||
-                                "https://media.idownloadblog.com/wp-content/uploads/2017/03/Twitter-new-2017-avatar-001.png"
-                            }
-                            alt=""
-                        />
-                    </Link>
-                </div>
-                                        <Link href={`/tweets/${tweet._id}?liked=${isLiked}`} className="flex space-x-4 flex-1">
+            <div className="w-12 h-12 bg-gray-700 rounded-full flex-shrink-0">
+                <Link href={`/users/${tweet.author._id}`}>
+                    <img
+                        className="w-12 h-12 rounded-full object-cover"
+                        src={
+                            tweet.author.image ||
+                            "https://media.idownloadblog.com/wp-content/uploads/2017/03/Twitter-new-2017-avatar-001.png"
+                        }
+                        alt=""
+                    />
+                </Link>
+            </div>
 
-                <div className="flex-1">
-                    <div className="text-gray-400 text-sm">
-                        @{tweet.author.username || tweet.author.name} · {createdTimeAgo}
+            <div className="flex-1">
+                <Link href={`/tweets/${tweet._id}?liked=${isLiked}`}>
+                    <div>
+                        <div className="text-gray-400 text-sm">
+                            @{tweet.author.username || tweet.author.name} · {createdTimeAgo}
+                        </div>
+
+                        <p className="mt-2">{tweet.content}</p>
                     </div>
+                </Link>
 
-                    <p className="mt-2">{tweet.content}</p>
+                {/* Actions at bottom */}
+                <div className="mt-4 flex justify-between max-w-md text-gray-500">
+                    <button
+                        onClick={() => setShowComments((prev) => !prev)}
+                        className="flex items-center gap-2 hover:text-sky-500"
+                    >
+                        <MessageCircle size={18} />
+                        {tweet.replies.length}
+                    </button>
+
+                    <button
+                        className="flex items-center gap-2 hover:text-pink-500"
+                        onClick={handleLikeAction}
+                    >
+                        {isLiked ? (
+                            <Heart size={22} color="#F23299" fill="#F23299" />
+                        ) : (
+                            <Heart size={22} />
+                        )}
+                        {likesCount}
+                    </button>
                 </div>
-            </Link>
-
-            <button
-                className="ml-auto hover:text-pink-500 transition text-gray-500 cursor-pointer mr-4"
-                onClick={handleLikeAction}
-            >
-                {isLiked ? (
-                    <Heart size={22} color="#F23299" fill="#F23299" />
-                ) : (
-                    <Heart size={22} />
-                )}
-            </button>
+            </div>
         </div>
     );
 }
