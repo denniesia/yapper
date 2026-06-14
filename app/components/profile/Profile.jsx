5bceda44d52942106d4ed6b6d5c858490924d861
dennie <denitsa.sureva@icloud.com>
2026-06-14 18:53:28 +0200

    only own profile can be edited

--- app/components/profile/Profile.jsx
+++ app/components/profile/Profile.jsx
@@ -4,17 +4,22 @@ import { useEffect, useState } from "react";
 import ProfileEditModal from "./ProfileEditModal";
 import TweetCard from "../tweet/TweetCard"
 import { useRouter } from "next/navigation";
+import useCurrentUser from "../../hooks/useCurrentUser";
 
 
 
 
-export default function Profile({ user }) {
+export default function Profile({ user: profileUser }) {
+    const { user: currentUser } = useCurrentUser();
     const [showEditProfileModal, setShowEditProfileModal] = useState(false);
     const [userTweets, setUserTweets] = useState([]);
 
     const router = useRouter();
 
-    const createdAt = new Date(user.createdAt).toLocaleDateString("en-GB", {
+    const isOwnProfile =
+        currentUser?._id === profileUser?._id;
+
+    const createdAt = new Date(profileUser.createdAt).toLocaleDateString("en-GB", {
         day: "2-digit",
         month: "short",
         year: "numeric",
@@ -24,7 +29,7 @@ export default function Profile({ user }) {
         async function fetchTweets() {
             try {
                 const res = await fetch(
-                    `http://localhost:3001/api/tweets/user/${user._id}`
+                    `http://localhost:3001/api/tweets/user/${profileUser._id}`
                 );
 
                 if (!res.ok) {
@@ -38,14 +43,15 @@ export default function Profile({ user }) {
                 console.error(err);
             }
         }
-        if (user?._id) {
+        if (profileUser?._id) {
             fetchTweets();
         }
-    }, [user]);
+    }, [profileUser]);
+
 
     const handleSaveProfile = async (data) => {
         try {
-            const res = await fetch(`http://localhost:3001/api/users/${user._id}`, {
+            const res = await fetch(`http://localhost:3001/api/users/${profileUser._id}`, {
                 method: "PATCH",
                 headers: {
                     "Content-Type": "application/json",
@@ -71,21 +77,20 @@ export default function Profile({ user }) {
         }
     };
 
-    console.log(userTweets)
     return (
 
         <div className="flex-1 border-r border-gray-800 max-w-3xl">
             <div className="flex-1 border-r border-gray-800 max-w-3xl">
                 {/* Header */}
                 <div className="sticky top-0 z-10 bg-black/80 backdrop-blur border-b border-gray-800 px-4 py-3">
-                    <h1 className="text-2xl font-bold ml-4">{user.name || user.username}</h1>
-                    <p className="text-sm text-gray-500 ml-4">{user.tweets?.length} posts</p>
+                    <h1 className="text-2xl font-bold ml-4">{profileUser.name || profileUser.username}</h1>
+                    <p className="text-sm text-gray-500 ml-4">{profileUser.tweets?.length} posts</p>
                 </div>
 
                 {/* Banner */}
                 <div className="h-52 bg-gray-700 relative">
                     <img
-                        src={user.banner || "https://images.unsplash.com/photo-1729575846511-f499d2e17d79?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+                        src={profileUser.banner || "https://images.unsplash.com/photo-1729575846511-f499d2e17d79?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
                         alt="Banner"
                         className="w-full h-full object-cover"
                     />
@@ -93,7 +98,7 @@ export default function Profile({ user }) {
                     {/* Profile Image */}
                     <div className="absolute -bottom-18 left-4">
                         <img
-                            src={user.image || 'https://media.idownloadblog.com/wp-content/uploads/2017/03/Twitter-new-2017-avatar-001.png'}
+                            src={profileUser.image || 'https://media.idownloadblog.com/wp-content/uploads/2017/03/Twitter-new-2017-avatar-001.png'}
                             alt="Profile"
                             className="w-36 h-36 rounded-full border-4 border-black object-cover"
                         />
@@ -102,13 +107,16 @@ export default function Profile({ user }) {
 
                 {/* Profile Info */}
                 <div className="px-4 pt-10 pb-4 border-b border-gray-800">
-                    <div className="flex justify-end">
-                        <button
-                            onClick={() => setShowEditProfileModal(true)}
-                            className="border border-gray-600 px-5 py-2 rounded-full font-semibold hover:bg-gray-900 transition">
-                            Edit profile
-                        </button>
-                    </div>
+                    {isOwnProfile && (
+                        <div className="flex justify-end">
+                            <button
+                                onClick={() => setShowEditProfileModal(true)}
+                                className="border border-gray-600 px-5 py-2 rounded-full font-semibold hover:bg-gray-900 transition">
+                                Edit profile
+                            </button>
+                        </div>
+                        )
+                    }
 
                     {
                         showEditProfileModal && (
@@ -120,18 +128,18 @@ export default function Profile({ user }) {
                         )
                     }
 
-                    <div className="mt-2">
-                        <h2 className="text-2xl font-bold">@{user?.username}</h2>
-                        <p className="text-gray-500 mt-1">{user?.email}</p>
+                    <div className="mt-8">
+                        <h2 className="text-2xl font-bold">@{profileUser?.username}</h2>
+                        <p className="text-gray-500 mt-1">{profileUser?.email}</p>
                     </div>
 
                     <p className="mt-3 text-gray-200">
-                        {user.bio}
+                        {profileUser.bio}
                     </p>
 
                     <div className="flex flex-wrap gap-4 text-gray-500 text-sm mt-3">
-                        {user.location && <div>📍 {user.location}</div>}
-                        {user.createdAt && <div>📅 Joined {createdAt}</div>}
+                        {profileUser.location && <div>📍 {profileUser.location}</div>}
+                        {profileUser.createdAt && <div>📅 Joined {createdAt}</div>}
                     </div>
 
                     {/* <div className="flex gap-5 mt-4 text-sm">
