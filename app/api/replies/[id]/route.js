3383bd6dcb3dc8e7119ece2a43b585e44f706cea
dennie <denitsa.sureva@icloud.com>
2026-06-15 17:10:10 +0200

    patch method on reply

--- app/api/replies/[id]/route.js
+++ app/api/replies/[id]/route.js
@@ -1,3 +1,4 @@
+import { NextResponse } from "next/server";
 import { makeSureDbIsReady } from "../../../lib/db";
 import Reply from "../../../lib/schemas/ReplySchema";
 
@@ -24,5 +25,37 @@ export async function DELETE(req, { params }) {
 		console.log("GET ERROR:", error);
 
 		return Response.json({ error: error.message }, { status: 500 });
+	}
+}
+
+
+export async function PATCH(req, { params }) {
+	try {
+		await makeSureDbIsReady();
+
+		const { id } = await params;
+		const { content } = await req.json();
+		
+		const updatedReply = await Reply.findByIdAndUpdate(
+			id, 
+			{
+				content
+			},
+			{ new: true }
+		)
+
+		if (!updatedReply) {
+			return NextResponse.json(
+				{ message: "Reply not found" },
+				{ status: 404 }
+			)
+		}
+
+		return NextResponse.json(updatedReply);
+	} catch(error) {
+		return NextResponse.json(
+			{ message : error.message },
+			{ status: 500 }
+		)
 	}
 }
