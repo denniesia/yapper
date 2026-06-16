030bec1d6c754fc03f1e5cc6d843ab26d33555ea
dennie <denitsa.sureva@icloud.com>
2026-06-16 19:35:41 +0200

    mongo atlas

--- app/lib/db.js
+++ app/lib/db.js
@@ -4,24 +4,26 @@ import assert from "node:assert";
 // caching for local development
 let cached = global.mongoose;
 if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+	cached = global.mongoose = { conn: null, promise: null };
 }
 
 export async function makeSureDbIsReady() {
-  if (cached.conn) {
-    return cached.conn;
-  }
+	if (cached.conn) {
+		return cached.conn;
+	}
 
-  const MONGODB_URI = 'mongodb://localhost:27017/yapper';  //process.env.MONGODB_URI;
-  assert(
-    MONGODB_URI,
-    "Please define the MONGODB_URI environment variable inside .env.local"
-  );
+	const MONGODB_URI = process.env.MONGODB_URI;
+	assert(
+		MONGODB_URI,
+		"Please define the MONGODB_URI environment variable inside .env.local",
+	);
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
-  }
+	if (!cached.promise) {
+		cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
+	}
+	
 
-  cached.conn = await cached.promise;
-  return cached.conn;
+	cached.conn = await cached.promise;
+	return cached.conn;
+
 }
