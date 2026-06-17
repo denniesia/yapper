1ee1cfe2143bc0895555c6fefac4a609b3ac929e
dennie <denitsa.sureva@icloud.com>
2026-06-17 21:30:07 +0200

    fixing import

--- app/users/[id]/page.js
+++ app/users/[id]/page.js
@@ -4,13 +4,14 @@ import Profile from "../../components/profile/Profile";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "../../api/auth/[...nextauth]/route";
+import { getUser } from "../../lib/getUser";
 
 export default async function ProfilePage({ params }) {
 	const { id } = await params;
 
 	const user = await getUser(id);
 
-
+console.log(user)
 	return (
 		<div className="flex min-h-screen bg-black text-white">
 			<Sidebar />
